4e0dfc04505658f994224990fe16d596d5bc09a4
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-10-23 22:01:17 +0530

    create blank Pixel file

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snake_game/blank_pixel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,26 +9,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // grid dimensions
+  int rowSize = 10;
+  int totalNumberOfSquares = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: [
           //high scores
-          Expanded(
-              child: Container(
-            color: Colors.blue,
-          )),
+          Expanded(child: Container()),
           // game grid
           Expanded(
-              child: Container(
-            color: Colors.green,
-          )),
+            flex: 3,
+            child: GridView.builder(
+              itemCount: totalNumberOfSquares,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: rowSize),
+              itemBuilder: (context, index) {
+                return const BlankPixel();
+              },
+            ),
+          ),
           // play button
-          Expanded(
-              child: Container(
-            color: Colors.blue,
-          )),
+          Expanded(child: Container()),
         ],
       ),
     );
