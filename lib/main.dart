05f5c6a5fe7de466e9f7db943904b0c8b3287460
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-11-02 22:54:08 +0530

    add with firebse console

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:snake_game/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCp3BkWX5h_P2tuxJW_fvfMRCtHlaB9s0g",
+        authDomain: "snakegame-59688.firebaseapp.com",
+        projectId: "snakegame-59688",
+        storageBucket: "snakegame-59688.appspot.com",
+        messagingSenderId: "12526319049",
+        appId: "1:12526319049:web:fa5ad7db4cab3e5821e418",
+        measurementId: "G-LL1D7REY5P"
+    )
+  );
   runApp(const MyApp());
 }
 
